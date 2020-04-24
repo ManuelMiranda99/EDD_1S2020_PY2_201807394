f51d1d14b8e7a68369862e79dd7d482fd0e117de
Manuel Miranda <3057476220301@ingenieria.usac.edu.gt>
2020-04-23 22:02:46 -0600

    Inicio
    Inicio de la realización del proyecto 2 de estructuras de datos

---
+++ Project2/src/project2/Project2.java
@@ -0,0 +1,22 @@
+/*
+----------------------------JAVA----------------------------
+
+Code made by: Angel Manuel Miranda Asturias
+Done on: ${date?date?string("yyyy")}
+
+This program is free software: you can redistribute it or modify
+
+Use it as you like and have fun reading it ^^
+
+------------------------------------------------------------
+*/
+
+package project2;
+
+public class Project2 {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
