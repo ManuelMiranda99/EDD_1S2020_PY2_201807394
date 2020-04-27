65651f445bcb7625e11193b429bd3627586167fa
Manuel Miranda <3057476220301@ingenieria.usac.edu.gt>
2020-04-27 13:53:44 -0600

    Estructuras de Datos
    Agregadas las clases de las estructuras de datos a utilizar.

---
+++ Project2/src/DS/BTree/BTree.java
@@ -0,0 +1,21 @@
+/*
+----------------------------JAVA----------------------------
+
+Code made by: Angel Manuel Miranda Asturias
+Done on: 2020
+
+This program is free software: you can redistribute it or modify
+
+Use it as you like and have fun reading it ^^
+
+------------------------------------------------------------
+ */
+package DS.BTree;
+
+/**
+ *
+ * @author manuel
+ */
+public class BTree {
+    
+}
